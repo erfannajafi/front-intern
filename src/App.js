09c9f4fa127a049f736502c894ed38b730d733cf
erfannajafi <najafi.e79@gmail.com>
2022-07-17 10:02:51 +0430

    show current time

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [time, setTime] = useState();
+  const [time, setTime] = useState(new Date().toLocaleString());
+
   useEffect(() => {
-    let timer = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
+    let timer = setInterval( () => {
+        setTime(new Date().toLocaleString())
+    },1000)
 
     return () => clearInterval(timer);
   }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{time}</p>
+        <div>{time}</div>
       </header>
     </div>
   );
